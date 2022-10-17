111cfc4890b5000fbd865ed7810faac0fe56e738
Victor <ovictoravila@gmail.com>
2022-10-16 22:26:28 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles/global.css";
+
+export function App() {
+  return (
+    <>
+      <h1 className="font-bold text-2xl text-gray-900">Hello World</h1>
+      <button className="bg-cyan-500 rounded px-4 py-2 font-medium text-white hover:bg-cyan-300">
+        Enviar
+      </button>
+    </>
+  );
+}
---
+++ src/components/TextInput.tsx
@@ -0,0 +1,38 @@
+import { Slot } from "@radix-ui/react-slot";
+import { InputHTMLAttributes, ReactNode } from "react";
+
+export interface TextInputRootProps {
+  children: ReactNode;
+}
+const TextInputRoot = ({ children }: TextInputRootProps) => {
+  return (
+    <div className="flex items-center gap-3 h-12 py-4 px-3 bg-gray-800 w-full rounded focus-within:ring-2 ring-cyan-300">
+      {children}
+    </div>
+  );
+};
+
+export interface TextInputIconProps {
+  children: ReactNode;
+}
+const TextInputIcon = ({ children }: TextInputIconProps) => {
+  return <Slot className="w-6 h-6 text-gray-400">{children}</Slot>;
+};
+
+export interface TextInputInputProps
+  extends InputHTMLAttributes<HTMLInputElement> {}
+
+const TextInputInput = (props: TextInputInputProps) => {
+  return (
+    <input
+      className="bg-transparent flex-1  text-gray-100 text-xs placeholder:text-gray-400 outline-none"
+      {...props}
+    />
+  );
+};
+
+export const TextInput = {
+  Root: TextInputRoot,
+  Input: TextInputInput,
+  Icon: TextInputIcon,
+};
